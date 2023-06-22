878a9dda5777a70aeba4dec55ccee88cb0e6ea29
gsnair <gsnair@adobe.com>
2023-06-22 16:41:57 +0530

    price based filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,11 +2,28 @@ import styles from "./Filter.module.scss"
 
 import Accordion from "../Utils/Accordion/Accordion"
 
-function Filter({ fetchBasedOnCategory }) {
+import { useState } from "react";
 
+function Filter({ fetchBasedOnCategory, fetchBasedOnPrice }) {
+
+   const [checked, setChecked] = useState([]);
+
+   const handleCheck = (event) => {
+      var updatedList = [...checked];
+      if (event.target.checked) {
+         updatedList = [...checked, event.target.value];
+      } else {
+         updatedList.splice(checked.indexOf(event.target.value), 1);
+      }
+      setChecked(updatedList);
+      fetchBasedOnPrice(updatedList)
+   };
+
+   const priceList = [{ min: 0, max: 10 }, { min: 10, max: 20 }, { min: 20, max: 50 }, { min: 50, max: 100 }, { min: 100, max: 1000 }]
 
    return (<div className={styles.container}>
       <ul>
+         <li><button category="all" onClick={fetchBasedOnCategory}>All</button></li>
          <li><button category="electronics" onClick={fetchBasedOnCategory}>Electronics</button></li>
          <li><button category="jewelery" onClick={fetchBasedOnCategory}>Jewelery</button></li>
          <li><button category="men's clothing" onClick={fetchBasedOnCategory}>Men's clothing</button></li>
@@ -14,10 +31,18 @@ function Filter({ fetchBasedOnCategory }) {
       </ul>
       <Accordion title="Shop by Price">
          <ul>
-            <li><button category="electronics" onClick={fetchBasedOnCategory}>Electronics</button></li>
-            <li><button category="jewelery" onClick={fetchBasedOnCategory}>Jewelery</button></li>
-            <li><button category="men's clothing" onClick={fetchBasedOnCategory}>Men's clothing</button></li>
-            <li><button category="women's clothing" onClick={fetchBasedOnCategory}>Women's clothing</button></li>
+            <form className={styles.price_checkbox}>
+               {
+                  priceList.map((item, index) => (
+                     <div key={index}>
+                        <label>
+                           <input value={item.min + "-" + item.max} type="checkbox" onChange={handleCheck} />
+                           <span>{`₹ ${item.min}.00 - ₹ ${item.max}.00`}</span>
+                        </label>
+                     </div>
+                  ))
+               }
+            </form>
          </ul>
       </Accordion>
       <Accordion title="Rating">
@@ -28,31 +53,6 @@ function Filter({ fetchBasedOnCategory }) {
             <li><button category="women's clothing" onClick={fetchBasedOnCategory}>Women's clothing</button></li>
          </ul>
       </Accordion>
-      <Accordion title="Category">
-         <ul>
-            <li><button category="electronics" onClick={fetchBasedOnCategory}>Electronics</button></li>
-            <li><button category="jewelery" onClick={fetchBasedOnCategory}>Jewelery</button></li>
-            <li><button category="men's clothing" onClick={fetchBasedOnCategory}>Men's clothing</button></li>
-            <li><button category="women's clothing" onClick={fetchBasedOnCategory}>Women's clothing</button></li>
-         </ul>
-      </Accordion>
-      <Accordion title="Category">
-         <ul>
-            <li><button category="electronics" onClick={fetchBasedOnCategory}>Electronics</button></li>
-            <li><button category="jewelery" onClick={fetchBasedOnCategory}>Jewelery</button></li>
-            <li><button category="men's clothing" onClick={fetchBasedOnCategory}>Men's clothing</button></li>
-            <li><button category="women's clothing" onClick={fetchBasedOnCategory}>Women's clothing</button></li>
-         </ul>
-      </Accordion>
-      <Accordion title="Category">
-         <ul>
-            <li><button category="electronics" onClick={fetchBasedOnCategory}>Electronics</button></li>
-            <li><button category="jewelery" onClick={fetchBasedOnCategory}>Jewelery</button></li>
-            <li><button category="men's clothing" onClick={fetchBasedOnCategory}>Men's clothing</button></li>
-            <li><button category="women's clothing" onClick={fetchBasedOnCategory}>Women's clothing</button></li>
-         </ul>
-      </Accordion>
-
    </div>);
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,58 +1,127 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 
 import Card from "../Utils/Card/Card";
 import Filter from "../Filter/Filter";
 
 import styles from "./Main.module.scss"
 
-const productURL = "https://fakestoreapi.com/products"
+let productURL = "https://fakestoreapi.com/products"
+
+
+const initialState = {
+    data: [],
+    loading: true,
+    error: null
+}
+
+const getResponse = async (url) => {
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "LOADING":
+            return {
+                ...state,
+                loading: true
+            }
+
+        case 'SUCCESS':
+            return {
+                data: action.data,
+                loading: false,
+                error: null
+            }
+
+        case "ERROR":
+            console.error(action.error)
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            }
+
+        default:
+            return state
+    }
+}
+
 
 function Main() {
-
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState([]);
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const { data, loading, error } = state;
 
     function fetchBasedOnCategory(e) {
-        setLoading(true)
-        fetch(`https://fakestoreapi.com/products/category/${e.target.attributes.category.value}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data);
-                setLoading(false)
+        dispatch({ type: "LOADING" })
+        if (e === "all" || e.target.attributes.category.value === "all") {
+            productURL = "https://fakestoreapi.com/products"
+        } else {
+            productURL = `https://fakestoreapi.com/products/category/${e.target.attributes.category.value}`
+        }
+        getResponse(productURL).then(data => {
+            dispatch({ type: "SUCCESS", data: data })
+        }).catch(error => dispatch({ type: "ERROR", error: error }))
+    }
+
+    function fetchBasedOnPrice(ranges) {
+        if (ranges.length === 0) {
+            fetchBasedOnCategory("all")
+        } else {
+            dispatch({ type: "LOADING" })
+            const minMaxPrices = []
+            ranges.forEach((range) => {
+                const rangeSplited = range.split("-")
+                minMaxPrices.push({
+                    min: parseInt(rangeSplited[0]),
+                    max: parseInt(rangeSplited[1])
+                })
             })
-            .catch((err) => {
-                console.log(err.message);
-            });
+            const tempData = []
+            let products = []
+            getResponse(productURL).then(data => {
+                products = data;
+                minMaxPrices.forEach((price) => {
+                    products.forEach(item => {
+                        if (item.price >= price.min && item.price <= price.max) tempData.push(item)
+                    })
+                })
+                dispatch({ type: "SUCCESS", data: tempData })
+            }).catch(error => dispatch({ type: "ERROR", error: error }))
+        }
     }
 
     useEffect(() => {
-        fetch(productURL)
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data);
-                setLoading(false)
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
+
+        dispatch({ type: "LOADING" })
+
+        getResponse(productURL).then(data => {
+            dispatch({ type: "SUCCESS", data: data })
+        }).catch(error => dispatch({ type: "ERROR", error: error }))
+
     }, []);
 
 
     const storeItems = data.map(item =>
         <Card key={item.id} title={item.title} img={item.image} cat={item.category} price={item.price} />)
     return (
-        <div className={`main ${styles.main}`}>
-            <div>
-                <h1>Summer Shop(20)</h1>
-            </div>
-            <div className={styles.store}>
-                <Filter fetchBasedOnCategory={fetchBasedOnCategory} />
-                <div className={styles.store_item_container}>
-                    {loading && <div className={styles.loading_modal} />}
-                    {storeItems}
+        error ? <h1>Error</h1> :
+            <div className={`main ${styles.main}`}>
+                <div>
+                    <h1>Summer Shop(20)</h1>
                 </div>
-            </div>
-        </div>);
+                <div className={styles.store}>
+                    <Filter fetchBasedOnCategory={fetchBasedOnCategory} fetchBasedOnPrice={fetchBasedOnPrice} />
+                    <div className={styles.store_item_container}>
+                        {loading && <div className={styles.loading_modal} />}
+                        {storeItems}
+                    </div>
+                </div>
+            </div>);
 }
 
 export default Main;
